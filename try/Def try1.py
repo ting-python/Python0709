50b91ad91d9bcd8bc57c867ac98fc67d0f46a8e2
MB-study <nk100012@nkes.tyc.edu.tw>
2020-07-14 20:27:49 +0800

    Python

---
+++ try/Def try1.py
@@ -0,0 +1,6 @@
+def a():
+    n = 1
+    h = 87
+    k = n + h
+    print(k)
+a()
---
+++ try/Def try2.py
@@ -0,0 +1,16 @@
+def a(n,h):
+    k = n + h
+    return k # print(k)結果不一樣
+ans = a(8,6)
+print(ans)
+
+ans = a(99,24)
+print(ans)
+
+ans = a(75,21)
+print(ans)
+
+ans = a(34,7)
+print(ans)
+
+
