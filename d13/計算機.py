537bc9f54d203241f3d61937a55e6b81eb8dc24e
ting <67962032+ting-python@users.noreply.github.com>
2020-08-27 21:38:35 +0800

    Add files via upload

--- d13/計算機.py
+++ d13/計算機.py
@@ -4,6 +4,22 @@ import tkinter.ttk as ttk
 win = tkinter.Tk()
 win.title('計算機')
 
+
+def dotcheck():
+    s = var.get()
+    if s[len(s) - 1] == '.' \
+        or s[len(s) - 1] == '+' \
+        or s[len(s) - 1] == '-' \
+        or s[len(s) - 1] == 'x' \
+        or s[len(s) - 1] == '/' :
+        print("最後不能為符號")
+        return False
+    elif s[len(s) - 1] and s[len(s) - 2] == '.':
+        print("小數點只有一個")
+        return False
+    else:
+        return True
+
 # 按鈕功能設定
 # C  <-  .  =
 def C():
@@ -14,7 +30,11 @@ def back():
 def dot():
     var.set(var.get() + ".")
 def equal():
-    var.set("%.2f" % eval(var.get()))
+    if dotcheck() == True:
+        var.set("%.2f" % eval(var.get()))
+    else:
+        print("請重新輸入")
+        print("__________________________")
 
 # +  -  *  /
 def plus():
@@ -22,31 +42,34 @@ def plus():
 def minus():
     var.set(var.get() + "-")
 def multiply():
-    var.set(var.get() + "x")
+    var.set(var.get() + "*")
 def devide():
     var.set(var.get() + "/")
+def pow():
+    s = var.get()
+    var.set(s + '*' + s)
 
 # 0 ~ 9
 def b0():
-    var.set(var.get() + 0)
+    var.set(var.get() + '0' if var.get() != '0' else '0')
 def b1():
-    var.set(var.get() + 1)
+    var.set(var.get() + '1' if var.get() != '0' else '1')
 def b2():
-    var.set(var.get() + 2)
+    var.set(var.get() + '2' if var.get() != '0' else '2')
 def b3():
-    var.set(var.get() + 3)
+    var.set(var.get() + '3' if var.get() != '0' else '3')
 def b4():
-    var.set(var.get() + 4)
+    var.set(var.get() + '4' if var.get() != '0' else '4')
 def b5():
-    var.set(var.get() + 5)
+    var.set(var.get() + '5' if var.get() != '0' else '5')
 def b6():
-    var.set(var.get() + 6)
+    var.set(var.get() + '6' if var.get() != '0' else '6')
 def b7():
-    var.set(var.get() + 7)
+    var.set(var.get() + '7' if var.get() != '0' else '7')
 def b8():
-    var.set(var.get() + 8)
+    var.set(var.get() + '8' if var.get() != '0' else '8')
 def b9():
-    var.set(var.get() + 9)
+    var.set(var.get() + '9' if var.get() != '0' else '9')
 
 
 # 按鈕設定
@@ -56,7 +79,7 @@ ori.configure('My.TButton', font=('Arial', 15))
 
 # Lable值設定
 var = tkinter.StringVar()
-var.set(int(0))
+var.set('0')
 my_lable = ttk.Label(win, textvariable=var,font=('Arial', 30), anchor=tkinter.E)
 
 # C  <-  .  =
@@ -70,6 +93,7 @@ plus = ttk.Button(win, text='+', style='My.TButton', command=plus)
 minus = ttk.Button(win, text='-', style='My.TButton', command=minus)
 mutiply = ttk.Button(win, text='*', style='My.TButton', command=multiply)
 devide = ttk.Button(win, text='/', style='My.TButton', command=devide)
+pow = ttk.Button(win, text='**', style='My.TButton', command=pow)
 
 # 0 ~ 9
 btn1 = ttk.Button(win, text='1', style='My.TButton', command=b1)
@@ -94,10 +118,11 @@ back.grid(row=1, column=2, rowspan=1, columnspan=1, sticky='EWNS')
 dot.grid(row=5, column=1, rowspan=1, columnspan=1, sticky='EWNS')
 equal.grid(row=5, column=2, rowspan=1, columnspan=1, sticky='EWNS')
 
-plus.grid(row=4, column=3, rowspan=2, columnspan=1, sticky='EWNS')
+plus.grid(row=4, column=3, rowspan=1, columnspan=1, sticky='EWNS')
 minus.grid(row=3, column=3, rowspan=1, columnspan=1, sticky='EWNS')
 mutiply.grid(row=2, column=3, rowspan=1, columnspan=1, sticky='EWNS')
 devide.grid(row=1, column=3, rowspan=1, columnspan=1, sticky='EWNS')
+pow.grid(row=5, column=3, rowspan=1, columnspan=1, sticky='EWNS')
 
 btn1.grid(row=4, column=0, rowspan=1, columnspan=1, sticky='EWNS')
 btn2.grid(row=4, column=1, rowspan=1, columnspan=1, sticky='EWNS')
